a60bccfd0ba2430c10e0055d893530fabfd1f149
Christian Brandalise <christian.brandalise@gmail.com>
2015-02-09 20:39:05 -0200

    fixed 'should confirm that when a function is attached to an object, 'this' refers to the object', and  'describe the in keyword'

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -44,8 +44,8 @@ describe("About Objects", function () {
       }
     };
    
-    expect(currentYear).toBe(FILL_ME_IN);
-    expect(meglomaniac.calculateAge()).toBe(FILL_ME_IN);
+    expect(currentYear).toBe(2015);
+    expect(meglomaniac.calculateAge()).toBe(2015-1970);
   });
 
   describe("'in' keyword", function () {
@@ -62,7 +62,7 @@ describe("About Objects", function () {
 
       var hasBomb = "theBomb" in meglomaniac;
      
-      expect(hasBomb).toBe(FILL_ME_IN);
+      expect(hasBomb).toBe(true);
     });
 
     it("should not have the detonator however", function () {
